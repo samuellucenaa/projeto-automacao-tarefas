8b124d41f80b4ef178446062868995b9e79d2542
samuellucenaa <samuellucenaa1@gmail.com>
2024-10-08 17:21:39 -0300

    automatizando o cadastro de produtos

--- main.py
+++ main.py
@@ -29,4 +29,52 @@ print(tabela)
 
 # passo 4: cadastrar produto
 
+linha = 0
+
+# para cada linha da minha tabela
+for linha in tabela.index: 
+
+#index é o n° das linhas, que o python faz automaticamente
+    
+    pyautogui.click(x=395, y=295)
+    
+    # codigo
+    codigo = tabela.loc[linha, 'codigo']
+    pyautogui.write(str(codigo))
+    pyautogui.press('tab')
+
+    # marca
+    marca = tabela.loc[linha, 'marca']
+    pyautogui.write(str(marca))
+    pyautogui.press('tab')
+
+    # tipo
+    tipo = tabela.loc[linha, 'tipo']
+    pyautogui.write(str(tipo))
+    pyautogui.press('tab')
+
+    # categoria
+    categoria = tabela.loc[linha, 'categoria']
+    pyautogui.write(int(categoria))
+    pyautogui.press('tab')
+
+    # preco unitario
+    preco_unitario = tabela.loc[linha, 'preco_unitario']
+    pyautogui.write(float(preco_unitario))
+    pyautogui.press('tab')
+
+    # custo
+    custo = tabela.loc[linha, 'custo']
+    pyautogui.write(float(custo))
+    pyautogui.press('tab')
+
+    # obs
+    obs = tabela.loc[linha, 'obs']
+    pyautogui.write(str(obs))
+    pyautogui.press('tab')
+
+    # clicar no enviar
+    pyautogui.press('enter')
+    pyautogui.scroll(5000)
+
 # passo 5: repetir processos
